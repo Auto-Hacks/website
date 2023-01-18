f04f2f0844d7baf7e54a31c1bd57e1cae92c740f
precious <kshitijjbbhatia@gmail.com>
2023-01-18 22:05:57 +0530

    add sponsor echo3d

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,5 +1,6 @@
 export default [
     {name: "wolfram", url: "https://wolfram.com" },
     {name: "care4parents", url: "https://www.care4parents.in/"},
-    {name: "paragon", url: "http://paragoncop.com/"}
+    {name: "paragon", url: "http://paragoncop.com/"},
+    {name: "echo3d", url: "https://www.echo3d.com/"}
 ]
